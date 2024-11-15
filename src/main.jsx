0d804ca51d1ac2d44622bdf0375b287cae5f0881
Alicja Bieryt <alicjabieryt@gmail.com>
2024-11-15 18:14:14 +0100

    chnged directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -11,5 +12,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
